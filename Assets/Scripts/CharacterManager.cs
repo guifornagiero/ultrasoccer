e603bae7bcff239a2414492635277f44ee1b37e8
vinihsilv <168356724+vinihsilv@users.noreply.github.com>
2025-05-04 13:49:35 -0300

    add: cena de seleção de personagens

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour
+{
+    public CharacterDatabase characterDB;
+    // Start is called before the first frame update
+    public Text nameText;
+    public SpriteRenderer artworkSprite;
+
+    private int selectedOption = 0;
+
+    void Start()
+    {
+        UpdateCharacter(selectedOption);
+    }
+
+    public void NextOption(){
+        selectedOption++;
+
+        if(selectedOption >= characterDB.CharacterCount){
+            selectedOption = 0;
+        }
+
+        UpdateCharacter(selectedOption);
+    }
+    public void BackOption(){
+        selectedOption--;
+        if(selectedOption < 0 ){
+            selectedOption = characterDB.CharacterCount -1;
+        }
+        UpdateCharacter(selectedOption);
+    }
+
+    private void UpdateCharacter(int selectedOption){
+        Character character = characterDB.GetCharacter(selectedOption);
+        artworkSprite.sprite = character.characterSprite;
+        nameText.text = character.characterName;
+    }
+}
